ed0cc7af86df5fee3427c9c8c5ebc97f73f5a354
tmfrl99 <99esg@naver.com>
2025-01-17 09:49:29 +0900

    first commit

---
+++ src/main/java/com/example/swapit/SwapitApplication.java
@@ -0,0 +1,13 @@
+package com.example.swapit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapitApplication.class, args);
+	}
+
+}
