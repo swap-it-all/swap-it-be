a72af4ffc16fd238d45d20b927ca10fe17972464
tmfrl99 <99esg@naver.com>
2025-01-24 18:09:04 +0900

    feat: Users 도메인 추가

---
+++ src/main/java/com/example/swapit/domain/Users.java
@@ -0,0 +1,37 @@
+package com.example.swapit.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "users_id", columnDefinition = "BIGINT", nullable = false)
+	private Long users_id;
+
+	@Column(name = "nickname", columnDefinition = "VARCHAR(20)", nullable = false)
+	private String nickname;
+
+	@Column(name = "profile_image_url", columnDefinition = "TEXT", nullable = false)
+	private String profile_image_url;
+
+	@Column(name = "email", columnDefinition = "VARCHAR(255)", nullable = false)
+	private String email;
+
+	@Column(name = "login_info", columnDefinition = "VARCHAR(20)", nullable = false)
+	private String login_info;
+}
