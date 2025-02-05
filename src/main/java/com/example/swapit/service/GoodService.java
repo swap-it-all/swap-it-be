3e601f8d3027e29d3bbe7d816fdcf3ac06d3ccb4
HJunng <guswjd1gh@gmail.com>
2025-02-05 20:24:39 +0900

    feat: 물건 Service 구현 (단건 조회, 생성, 수정, 삭제)

---
+++ src/main/java/com/example/swapit/service/GoodService.java
@@ -0,0 +1,22 @@
+package com.example.swapit.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.swapit.domain.dto.GoodsDetailDto;
+import com.example.swapit.domain.dto.GoodsListDto;
+import com.example.swapit.domain.dto.GoodsRequestDto;
+
+@Service
+public interface GoodService {
+
+	// 물건 조회
+	GoodsListDto getGoods();
+
+	GoodsDetailDto getGoodDetail(Long goodsId);
+
+	void insertGood(GoodsRequestDto goodsRequestDto);
+
+	void updateGood(Long goodsId, GoodsRequestDto goodsRequestDto);
+
+	void deleteGood(Long goodsId);
+}
---
+++ src/main/java/com/example/swapit/service/GoodServiceImpl.java
@@ -0,0 +1,85 @@
+package com.example.swapit.service;
+
+import com.example.swapit.common.exception.CustomException;
+import com.example.swapit.common.exception.ErrorCode;
+import com.example.swapit.domain.Categories;
+import com.example.swapit.domain.Goods;
+import com.example.swapit.domain.Users;
+import com.example.swapit.domain.dto.GoodsDetailDto;
+import com.example.swapit.domain.dto.GoodsListDto;
+import com.example.swapit.domain.dto.GoodsRequestDto;
+import com.example.swapit.repository.CategoriesRepository;
+import com.example.swapit.repository.GoodsRepository;
+import com.example.swapit.repository.UsersRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Transactional
+@RequiredArgsConstructor
+public class GoodServiceImpl implements GoodService {
+
+	private final GoodsRepository goodsRepository;
+	private final UsersRepository usersRepository;
+	private final CategoriesRepository categoriesRepository;
+
+	@Override
+	public GoodsListDto getGoods() {
+		return null;
+	}
+
+	@Override
+	public GoodsDetailDto getGoodDetail(Long goodsId) {
+		Goods good = goodsRepository.findById(goodsId)
+			.orElseThrow(() -> new CustomException(ErrorCode.GOOD_NOT_FOUND));
+		return GoodsDetailDto.of(good);
+	}
+
+	@Override
+	public void insertGood(GoodsRequestDto goodsRequestDto) {
+		Users user = findUser();
+		Categories category = categoriesRepository.findById(goodsRequestDto.getCategoryId())
+			.orElseThrow(() -> new CustomException(ErrorCode.CATEGORY_NOT_FOUND));
+
+		goodsRepository.save(goodsRequestDto.toEntity(user, category));
+	}
+
+	@Override
+	public void updateGood(Long goodsId, GoodsRequestDto goodsRequestDto) {
+		Goods good = goodsRepository.findById(goodsId)
+			.orElseThrow(() -> new CustomException(ErrorCode.GOOD_NOT_FOUND));
+		Users currentUser = findUser();
+
+		checkAuthorization(good.getUser(), currentUser);
+
+		Categories category = categoriesRepository.findById(goodsRequestDto.getCategoryId())
+			.orElseThrow(() -> new CustomException(ErrorCode.CATEGORY_NOT_FOUND));
+
+		good.update(goodsRequestDto, category);
+	}
+
+	@Override
+	public void deleteGood(Long goodsId) {
+		Goods good = goodsRepository.findById(goodsId)
+			.orElseThrow(() -> new CustomException(ErrorCode.GOOD_NOT_FOUND));
+		Users currentUser = findUser();
+
+		checkAuthorization(good.getUser(), currentUser);
+
+		goodsRepository.delete(good);
+	}
+
+	// todo: User 가져오는 부분 변경
+	private Users findUser() {
+		return usersRepository.findById(1L)
+			.orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+	}
+
+	private void checkAuthorization(Users goodsUser, Users currentUser) {
+		if (goodsUser.getUsersId().equals(currentUser.getUsersId())) {
+			throw new CustomException(ErrorCode.UNAUTHORIZED_ACCESS);
+		}
+	}
+}
