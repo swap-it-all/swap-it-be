680535ede4244ff253dde31788dcf76bdef70f55
HJunng <guswjd1gh@gmail.com>
2025-02-05 22:35:31 +0900

    feat : 에러 코드 추가 - 권한 확인, 카테고리 찾을 수 없음

--- src/main/java/com/example/swapit/common/exception/ErrorCode.java
+++ src/main/java/com/example/swapit/common/exception/ErrorCode.java
@@ -17,10 +17,13 @@ public enum ErrorCode {
 
 	// user error
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
+	UNAUTHORIZED_ACCESS(HttpStatus.FORBIDDEN, "권한이 없습니다."),
 
 	// goods error
-	GOOD_NOT_FOUND(HttpStatus.NOT_FOUND, "물건을 찾을 수 없습니다.");
+	GOOD_NOT_FOUND(HttpStatus.NOT_FOUND, "물건을 찾을 수 없습니다."),
 
+	// categories error
+	CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 카테고리를 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
