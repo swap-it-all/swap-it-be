fb3c1fbb07745446f457d4407bba26d71e2084de
HJunng <guswjd1gh@gmail.com>
2025-02-06 20:53:52 +0900

    fix : 오류 수정 (경로는 RequestMapping에 연결해야 함)

--- src/main/java/com/example/swapit/controller/GoodsController.java
+++ src/main/java/com/example/swapit/controller/GoodsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,8 @@ import com.example.swapit.service.GoodsService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class GoodsController {
 
