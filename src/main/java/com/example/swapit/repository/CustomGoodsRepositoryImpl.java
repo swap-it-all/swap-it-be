b4a310203eb858eb174d6cd98ed9f182349af77f
HJunng <guswjd1gh@gmail.com>
2025-02-06 20:49:54 +0900

    refactor :  불필요한 메서드 제거 후, Map 형태로 변경

--- src/main/java/com/example/swapit/repository/CustomGoodsRepositoryImpl.java
+++ src/main/java/com/example/swapit/repository/CustomGoodsRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Map;
 
 import com.example.swapit.domain.Goods;
 import com.example.swapit.domain.QGoods;
@@ -33,10 +34,22 @@ public class CustomGoodsRepositoryImpl implements CustomGoodsRepository {
 			? null
 			: qGoods.category.id.in(categoryIds);
 
+		// 정렬 기준 설정을 Map으로 관리
+		Map<String, OrderSpecifier<?>> orderByMap = Map.of(
+			"popular", qGoods.viewCount.desc(),
+			"recent", qGoods.createdAt.desc(),
+			"priceHigh", qGoods.price.desc(),
+			"priceLow", qGoods.price.asc()
+		);
+
+		// 정렬 기준이 없을 경우, 기본값 설정
+		String safeSortby = (sortBy == null) ? "popular" : sortBy;
+		OrderSpecifier<?> orderSpecifier = orderByMap.get(safeSortby);
+
 		// 동적 조건: 커서 (커서 기준 다음 거부터 조회)
 		BooleanExpression cursorCondition = (cursor == null)
 			? null // 처음 요청일 경우 커서 조건 제외
-			: switch (sortBy) {
+			: switch (safeSortby) {
 			case "popular" -> qGoods.viewCount.lt(cursor); // 인기순: 조회수 기준 커서
 			case "recent" -> qGoods.createdAt.lt(
 				LocalDateTime.ofInstant(Instant.ofEpochMilli(cursor), ZoneId.systemDefault())
@@ -46,15 +59,7 @@ public class CustomGoodsRepositoryImpl implements CustomGoodsRepository {
 			default -> qGoods.id.gt(cursor); // 기본값: 인기순
 		};
 
-		// 정렬 조건
-		OrderSpecifier<?> orderSpecifier = switch (sortBy) {
-			case "popular" -> qGoods.viewCount.desc(); // 인기순
-			case "recent" -> qGoods.createdAt.desc();  // 최신순
-			case "priceHigh" -> qGoods.price.desc();   // 가격 높은 순
-			case "priceLow" -> qGoods.price.asc();     // 가격 낮은 순
-			default -> qGoods.createdAt.desc();        // 기본값: 최신순
-		};
-
+		// todo : 나중에 쿼리 최적화 시, size+1개 가져오는 거랑 count() 서브쿼리로 다음 데이터가 있는지 확인하는 거 최적화 필요.
 		// 쿼리 실행
 		return queryFactory.selectFrom(qGoods)
 			.where(cursorCondition, keywordCondition, categoryCondition)
