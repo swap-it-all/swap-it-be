186a1f6c6723c54f2a9f25f67beea04fe0c511fc
tmfrl99 <99esg@naver.com>
2025-02-08 17:46:08 +0900

    chore: 테스트용 api 검증 필터 제외

--- src/main/java/com/example/swapit/config/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/swapit/config/security/jwt/JwtAuthenticationFilter.java
@@ -28,7 +28,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		"/api/user/auth/refresh",
 		"/api/all/auth/login/google",
 		"/api/all/auth/login/kakao",
-		"/api/user/auth/logout"
+		"/api/user/auth/logout",
+		"/api/all/sample"
 	);
 
 	@Override
