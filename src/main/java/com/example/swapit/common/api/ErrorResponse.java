0945771accd4b45e46b6a653c586c84a4025fc14
HJunng <guswjd1gh@gmail.com>
2025-02-10 01:16:25 +0900

    fix : 에러 발생 시, json 응답 안보이는 에러 해결

--- src/main/java/com/example/swapit/common/api/ErrorResponse.java
+++ src/main/java/com/example/swapit/common/api/ErrorResponse.java
@@ -3,11 +3,13 @@ package com.example.swapit.common.api;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 @JsonPropertyOrder({"status", "errorCode", "message"})
 public class ErrorResponse {
-	private final int status;
-	private final String errorCode;
-	private final String message;
+	private int status;
+	private String errorCode;
+	private String message;
 }
