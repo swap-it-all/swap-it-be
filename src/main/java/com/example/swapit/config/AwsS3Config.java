9a2c47e8b0e354ff2bffe8cf57c91aedc8dfead3
HJunng <guswjd1gh@gmail.com>
2025-02-12 23:22:43 +0900

    build : dev, prd에서의 Bean 주입 다르게 설정 (보안상 EC2 에서는 IAM Role을 그대로 가져와서 사용할 수 있음)

--- src/main/java/com/example/swapit/config/AwsS3Config.java
+++ src/main/java/com/example/swapit/config/AwsS3Config.java
@@ -1,9 +1,13 @@
 package com.example.swapit.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -11,10 +15,25 @@ import software.amazon.awssdk.services.s3.S3Client;
 public class AwsS3Config {
 
 	@Bean
-	public S3Client s3Client() {
+	@Profile("prd")
+	public S3Client prdS3Client() {
 		return S3Client.builder()
 			.region(Region.AP_NORTHEAST_2)
 			.credentialsProvider(InstanceProfileCredentialsProvider.create())
+			.build();
+	}
+
+	@Bean
+	@Profile("dev")
+	public S3Client devS3Client(
+		@Value("${cloud.aws.credentials.access-key}") String accessKey,
+		@Value("${cloud.aws.credentials.secret-key}") String secretKey
+	) {
+		return S3Client.builder()
+			.region(Region.AP_NORTHEAST_2)
+			.credentialsProvider(StaticCredentialsProvider.create(
+				AwsBasicCredentials.create(accessKey, secretKey)
+			))
 			.build();
 	}
 }
