5f934bab3880a0738807593c0cfc8cdc0b633d6b
HJunng <guswjd1gh@gmail.com>
2025-02-17 23:26:30 +0900

    fix : 알림 타입 enum 수정

--- src/main/java/com/example/swapit/domain/NotificationType.java
+++ src/main/java/com/example/swapit/domain/NotificationType.java
@@ -1,5 +1,16 @@
 package com.example.swapit.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum NotificationType {
-	TRADE_REQUEST, TRADE_ACCEPT, TRADE_REJECT, CHAT
+	REQUESTED("스왑 요청"),
+	ACCEPTED("스왑 수락"),
+	REJECTED("스왑 거절"),
+	COMPLETED("스왑 완료"),
+	REVIEW("리뷰 도착"),
+	CHAT("채팅");
+
+	private final String description;
 }
