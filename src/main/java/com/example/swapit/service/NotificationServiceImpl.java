d8a7b2aca7141485264a8bf17dae279d327f3ba4
HJunng <guswjd1gh@gmail.com>
2025-02-18 02:58:09 +0900

    feat : 트랜잭션 readonly로 변경

--- src/main/java/com/example/swapit/service/NotificationServiceImpl.java
+++ src/main/java/com/example/swapit/service/NotificationServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.swapit.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.swapit.domain.Users;
 import com.example.swapit.domain.dto.NotificationDto;
@@ -18,6 +19,7 @@ public class NotificationServiceImpl implements NotificationService {
 	private final CurrentUserService currentUserService;
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<NotificationDto> getMyNotifications() {
 		Users user = currentUserService.getCurrentUser();
 		return notificationRepository.findByUserAndReadNotOrderByCreatedAtDesc(user)
