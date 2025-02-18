e3a0bf19b8beb6852f8c9f8414b405be841ec8bb
HJunng <guswjd1gh@gmail.com>
2025-02-18 18:06:27 +0900

    feat : 유저 prefix 추가

--- src/main/java/com/example/swapit/config/websocket/WebSocketConfig.java
+++ src/main/java/com/example/swapit/config/websocket/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.example.swapit.config.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -21,10 +22,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableSimpleBroker("/topic", "/queue") // 구독 채널
+		registry.enableSimpleBroker("/topic", "/queue") // 일반 구독 채널
 			.setHeartbeatValue(new long[] {10000, 20000}) // 10초, 20초 간격으로 하트비트 전송
 			.setTaskScheduler(this.taskScheduler);
 		registry.setApplicationDestinationPrefixes("/app"); // 클라이언트가 보낼 prefix
+		registry.setUserDestinationPrefix("/user"); // 특정 사용자에게 보낼 때 사용하는 prefix
 	}
 
 	@Override
@@ -34,8 +36,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 			.setAllowedOrigins("*"); // cors
 	}
 
-	// @Override
-	// public void configureClientInboundChannel(ChannelRegistration registration) {
-	// 	registration.interceptors(stompHandler);
-	// }
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(stompHandler);
+	}
 }
