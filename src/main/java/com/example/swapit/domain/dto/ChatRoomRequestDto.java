9f89bf6ff1339ef027fc05f4c95edf89d6cc6e94
tmfrl99 <99esg@naver.com>
2025-02-18 23:27:55 +0900

    fix: validation 수정

--- src/main/java/com/example/swapit/domain/dto/ChatRoomRequestDto.java
+++ src/main/java/com/example/swapit/domain/dto/ChatRoomRequestDto.java
@@ -10,6 +10,6 @@ public class ChatRoomRequestDto {
 	@NotNull(message = "goodsId는 필수 입력 값입니다.")
 	private Long goodsId;
 
-	@NotNull(message = "goodsId는 필수 입력 값입니다.")
+	@NotNull(message = "requesterId는 필수 입력 값입니다.")
 	private Long requesterId;
 }
