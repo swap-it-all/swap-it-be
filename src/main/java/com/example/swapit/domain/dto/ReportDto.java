1aca1c5361d30e1923155bd1f4e9ffef938b3e7f
tmfrl99 <99esg@naver.com>
2025-02-19 16:04:11 +0900

    test: 신고 메일 전송 컨트롤러 테스트 코드 작성

--- src/main/java/com/example/swapit/domain/dto/ReportDto.java
+++ src/main/java/com/example/swapit/domain/dto/ReportDto.java
@@ -2,9 +2,11 @@ package com.example.swapit.domain.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ReportDto {
 	@NotNull(message = "reportedId는 필수 입력 값입니다.")
 	private Long reportedId;
