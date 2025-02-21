f1d99cd18143912e503c0a1085c64e72e40a215e
HJunng <guswjd1gh@gmail.com>
2025-02-22 03:19:54 +0900

    refactor : 사용하지 않는 메서드 삭제

--- src/main/java/com/example/swapit/controller/UserController.java
+++ src/main/java/com/example/swapit/controller/UserController.java
@@ -1,8 +1,5 @@
 package com.example.swapit.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.swapit.common.api.ApiResponse;
-import com.example.swapit.config.security.CustomUserDetails;
-import com.example.swapit.domain.Users;
 import com.example.swapit.domain.dto.UserNicknameDto;
 import com.example.swapit.domain.dto.UserPageDto;
-import com.example.swapit.domain.dto.UserResponseDTO;
 import com.example.swapit.service.UsersService;
 
 import jakarta.validation.Valid;
@@ -28,23 +22,6 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
 	private final UsersService usersService;
-
-	// todo : 현재 쓰고 있는 곳이 아예 없는지 확인 후, 삭제 필수.
-	// 사용자 정보 가져오는 임시 코드
-	@GetMapping("/user/me")
-	public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
-		if (customUserDetails == null) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 정보가 없습니다.");
-		}
-		Users user = customUserDetails.getUser();
-		UserResponseDTO userResponseDTO = UserResponseDTO.builder()
-			.nickname(user.getNickname())
-			.email(user.getEmail())
-			.profileImgUrl(user.getProfileImageUrl())
-			.loginInfo(user.getLoginInfo())
-			.build();
-		return ResponseEntity.ok(userResponseDTO);
-	}
 
 	@GetMapping("/user/auth/info/my")
 	public ApiResponse<UserPageDto> getMyPage() {
