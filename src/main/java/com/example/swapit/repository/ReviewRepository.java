f42c9b0a7771904116f211901a6bfacf04d66d17
HJunng <guswjd1gh@gmail.com>
2025-02-22 03:15:42 +0900

    fix : 리뷰가 하나도 없을 경우, 평점 평균이 null이 아니라 0.0이 되도록 수정

--- src/main/java/com/example/swapit/repository/ReviewRepository.java
+++ src/main/java/com/example/swapit/repository/ReviewRepository.java
@@ -13,8 +13,8 @@ import io.lettuce.core.dynamic.annotation.Param;
 public interface ReviewRepository extends JpaRepository<Reviews, Long> {
 	List<Reviews> findAllByRevieweeOrderByCreatedAtDesc(Users reviewee);
 
-	@Query("SELECT AVG(r.rating) FROM Reviews r WHERE r.reviewee = :reviewee")
-	double averageRatingByReviewee(@Param("reviewee") Users reviewee);
+	@Query("SELECT COALESCE(AVG(r.rating), 0.0) FROM Reviews r WHERE r.reviewee = :reviewee")
+	Double averageRatingByReviewee(@Param("reviewee") Users reviewee);
 
 	List<Reviews> findTop3ByRevieweeOrderByCreatedAtDesc(Users reviewee);
 }
