7710b4404b78c9493477a98de09e8544f1065490
HJunng <guswjd1gh@gmail.com>
2025-02-22 02:05:05 +0900

    feat : 유저 마이페이지 정보 dto에서 필요한 값을 repository에서 구현

--- src/main/java/com/example/swapit/repository/TradesRepository.java
+++ src/main/java/com/example/swapit/repository/TradesRepository.java
@@ -6,11 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.example.swapit.domain.Goods;
 import com.example.swapit.domain.Trades;
+import com.example.swapit.domain.Users;
 
 import io.lettuce.core.dynamic.annotation.Param;
 
 public interface TradesRepository extends JpaRepository<Trades, Long> {
 	long countByTargetGoodsIdAndIsDeletedFalse(Long goodsId);
+
+	@Query("SELECT COUNT(*) FROM Trades t WHERE ( t.owner = :user OR t.requester = :user ) AND t.status = 'COMPLETED'")
+	long countByCompletedTradesByUser(@Param("user") Users user);
 
 	@Modifying
 	@Query("UPDATE Trades t SET t.status = 'REJECTED' WHERE t.targetGoods = :targetGoods AND t.id <> :acceptedTradeId")
