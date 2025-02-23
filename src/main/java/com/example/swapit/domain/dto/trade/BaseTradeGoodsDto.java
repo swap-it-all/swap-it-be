c7dc9fe5001a1d7ae474b9b0f75b25f02d66772d
tmfrl99 <99esg@naver.com>
2025-02-23 20:58:19 +0900

    fix: abstract 키워드 추가

--- src/main/java/com/example/swapit/domain/dto/trade/BaseTradeGoodsDto.java
+++ src/main/java/com/example/swapit/domain/dto/trade/BaseTradeGoodsDto.java
@@ -1,20 +1,14 @@
 package com.example.swapit.domain.dto.trade;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-public class BaseTradeGoodsDto {
+@AllArgsConstructor
+public abstract class BaseTradeGoodsDto {
 	protected Long goodsId;
 	protected String title;
 	protected long price;
 	protected String category;
 	protected String placeName;
-
-	public BaseTradeGoodsDto(Long goodsId, String title, long price, String category, String placeName) {
-		this.goodsId = goodsId;
-		this.title = title;
-		this.price = price;
-		this.category = category;
-		this.placeName = placeName;
-	}
 }
