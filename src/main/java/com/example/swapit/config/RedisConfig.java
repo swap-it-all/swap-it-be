8435c115fa9a5a7247677b8c7e37ebf165ac003b
HJunng <guswjd1gh@gmail.com>
2025-02-26 14:28:14 +0900

    hotfix : redis config ssl 설정 및 타임 아웃 적용

--- src/main/java/com/example/swapit/config/RedisConfig.java
+++ src/main/java/com/example/swapit/config/RedisConfig.java
@@ -1,13 +1,20 @@
 package com.example.swapit.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
 
 @Configuration
 public class RedisConfig {
@@ -18,9 +25,23 @@ public class RedisConfig {
 	@Value("${spring.data.redis.port}")
 	private int redisPort;
 
+	@Value("${spring.data.redis.timeout}")
+	private int redisTimeout;
+
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(redisHost, redisPort);
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+
+		// Lettuce Client 설정 (SSL 및 타임아웃 적용)
+		LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+			.commandTimeout(Duration.ofMillis(redisTimeout)) // command 타임아웃 설정
+			.clientOptions(ClientOptions.builder()
+				.socketOptions(SocketOptions.builder().connectTimeout(Duration.ofSeconds(5)).build()) // 소켓 연결 타임아웃 5초
+				.build())
+			.useSsl() // TLS 적용
+			.build();
+
+		return new LettuceConnectionFactory(config, clientConfig);
 	}
 
 	@Bean
