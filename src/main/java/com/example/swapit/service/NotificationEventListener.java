069a66b6de1f1d04862e692afd2f04929578a5e3
HJunng <guswjd1gh@gmail.com>
2025-02-28 03:07:50 +0900

    feat : 웹소켓 알림에 실패했을 때, FCM 알림으로 전송

--- src/main/java/com/example/swapit/service/NotificationEventListener.java
+++ src/main/java/com/example/swapit/service/NotificationEventListener.java
@@ -10,11 +10,14 @@ import com.example.swapit.domain.NotificationEvent;
 import com.example.swapit.domain.Notifications;
 import com.example.swapit.domain.Users;
 import com.example.swapit.domain.dto.NotificationDto;
+import com.example.swapit.repository.FcmTokenRepository;
 import com.example.swapit.repository.NotificationRepository;
 import com.example.swapit.repository.UsersRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class NotificationEventListener {
@@ -22,6 +25,8 @@ public class NotificationEventListener {
 	private final SimpMessagingTemplate messagingTemplate;
 	private final NotificationRepository notificationRepository;
 	private final UsersRepository usersRepository;
+	private final FcmTokenRepository fcmTokenRepository;
+	private final FcmNotificationService fcmNotificationService;
 
 	@EventListener
 	public void handleNotification(NotificationEvent event) {
@@ -34,8 +39,22 @@ public class NotificationEventListener {
 		Notifications noti = event.toEntity(user);
 		notificationRepository.save(noti);
 
-		// websocket 실시간 알림 전송 : "/user/queue/notifications" 구독
-		messagingTemplate.convertAndSendToUser(userId.toString(), "/queue/notifications",
-			NotificationDto.of(noti));
+		// 웹소켓 알림 전송 (앱이 온라인 상태) : "/user/queue/notifications" 구독
+		boolean isWebsocketSent = false;
+		try {
+			messagingTemplate.convertAndSendToUser(userId.toString(), "/queue/notifications",
+				NotificationDto.of(noti));
+			isWebsocketSent = true;
+		} catch (Exception e) {
+			// todo : 이상 없으면 이후에 로그 제거
+			log.info("[알림LOG] 알림 웹소켓 전송 실패 -> FCM으로 전송. {}", e.getMessage());
+		}
+
+		// 앱이 백그라운드 상태일 때, FCM 알림 전송 (웹소켓 실패 or FCM 알림 필요)
+		if (!isWebsocketSent) {
+			fcmTokenRepository.findByUser(user).ifPresent(fcmToken -> {
+				fcmNotificationService.sendFcmNotification(fcmToken.getFcmToken(), noti);
+			});
+		}
 	}
 }
