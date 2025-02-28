1e12c7c3a3ba17d41fe16d77f4f71f319518985f
HJunng <guswjd1gh@gmail.com>
2025-03-01 01:44:44 +0900

    chore : 파일 위치 이동

--- src/test/java/com/example/swapit/service/TradesServiceTest.java
+++ src/test/java/com/example/swapit/service/TradesServiceTest.java
@@ -24,15 +24,16 @@ import com.example.swapit.domain.GoodsImages;
 import com.example.swapit.domain.TradeStatus;
 import com.example.swapit.domain.Trades;
 import com.example.swapit.domain.Users;
-import com.example.swapit.domain.dto.TradesRequestDto;
 import com.example.swapit.domain.dto.trade.MyGoodsDto;
 import com.example.swapit.domain.dto.trade.MyRequestDto;
 import com.example.swapit.domain.dto.trade.ReceivedRequestDto;
 import com.example.swapit.domain.dto.trade.RequestGoodsImageDto;
 import com.example.swapit.domain.dto.trade.TradeCountProjection;
+import com.example.swapit.domain.dto.trade.TradesRequestDto;
 import com.example.swapit.repository.GoodsImagesRepository;
 import com.example.swapit.repository.GoodsRepository;
 import com.example.swapit.repository.TradesRepository;
+import com.example.swapit.service.notification.NotificationEventPublisher;
 
 @ExtendWith(MockitoExtension.class)
 public class TradesServiceTest {
