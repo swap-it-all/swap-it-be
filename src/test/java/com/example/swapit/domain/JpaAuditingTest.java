07de4a3344caf6b2c60fd6287da36ced80e9b56e
HJunng <guswjd1gh@gmail.com>
2025-03-02 01:10:34 +0900

    test : test container 설정 되돌림.

--- src/test/java/com/example/swapit/domain/JpaAuditingTest.java
+++ src/test/java/com/example/swapit/domain/JpaAuditingTest.java
@@ -3,11 +3,9 @@ package com.example.swapit.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.example.swapit.config.TestConfig;
 import com.example.swapit.repository.CategoriesRepository;
@@ -17,8 +15,6 @@ import com.example.swapit.testcontainer.BaseIntegrationTest;
 
 @SpringBootTest
 @Import(TestConfig.class)
-@Testcontainers
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class JpaAuditingTest extends BaseIntegrationTest {
 
 	@Autowired
