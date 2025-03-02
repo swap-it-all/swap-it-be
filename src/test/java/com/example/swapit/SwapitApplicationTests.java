6d6b7c8fdbaebe2e8b2e4e8e4169ea61b97279e4
HJunng <guswjd1gh@gmail.com>
2025-03-02 20:25:18 +0900

    fix : 불필요한 테스트 클래스 삭제

--- src/test/java/com/example/swapit/SwapitApplicationTests.java
+++ src/test/java/com/example/swapit/SwapitApplicationTests.java
@@ -1,31 +1,15 @@
 package com.example.swapit;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 
 import com.example.swapit.config.TestConfig;
 import com.example.swapit.testcontainer.BaseIntegrationTest;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootTest
 @Import(TestConfig.class)
 class SwapitApplicationTests extends BaseIntegrationTest {
-
-	@Autowired
-	private ApplicationContext applicationContext;
-
-	@PostConstruct
-	public void printAllBeans() {
-		System.out.println("Listing all beans:");
-		String[] beanNames = applicationContext.getBeanDefinitionNames();
-		for (String beanName : beanNames) {
-			System.out.println("➡ Bean: " + beanName);
-		}
-	}
 
 	@Test
 	void contextLoads() {
--- src/test/java/com/example/swapit/testcontainer/BaseIntegrationTest.java
+++ src/test/java/com/example/swapit/testcontainer/BaseIntegrationTest.java
@@ -19,19 +19,9 @@ public abstract class BaseIntegrationTest {
 
 	@DynamicPropertySource
 	static void overrideProperties(DynamicPropertyRegistry registry) {
-		waitForDatabaseToBeReady();
 		registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
 		registry.add("spring.datasource.username", mysqlContainer::getUsername);
 		registry.add("spring.datasource.password", mysqlContainer::getPassword);
 		registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
-	}
-
-	private static void waitForDatabaseToBeReady() {
-		try {
-			Thread.sleep(5000); // hibernate가 MySQL 실행을 기다릴 수 있도록 5초 대기
-			System.out.println("Waiting for MySQL to be ready...");
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
 	}
 }
