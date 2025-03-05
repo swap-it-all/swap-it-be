2aff615cb1294414718e4340e363292c4c0ad26b
HJunng <guswjd1gh@gmail.com>
2025-03-05 16:47:03 +0900

    fix : dao 엔티티에서 받은 엔티티로 변경

--- src/main/java/com/example/swapit/service/TradesServiceImpl.java
+++ src/main/java/com/example/swapit/service/TradesServiceImpl.java
@@ -19,11 +19,11 @@ import com.example.swapit.domain.NotificationType;
 import com.example.swapit.domain.TradeStatus;
 import com.example.swapit.domain.Trades;
 import com.example.swapit.domain.Users;
+import com.example.swapit.domain.dao.TradeGoodsDao;
 import com.example.swapit.domain.dto.trade.InProgressCountDto;
 import com.example.swapit.domain.dto.trade.MyGoodsDto;
 import com.example.swapit.domain.dto.trade.MyRequestDto;
 import com.example.swapit.domain.dto.trade.ReceivedRequestDto;
-import com.example.swapit.domain.dto.trade.RequestGoodsImageDto;
 import com.example.swapit.domain.dto.trade.TradeCountProjection;
 import com.example.swapit.domain.dto.trade.TradeMyGoodsRequestDto;
 import com.example.swapit.domain.dto.trade.TradesRequestDto;
@@ -187,6 +187,7 @@ public class TradesServiceImpl implements TradesService {
 
 		// 거래 완료 처리
 		trade.setStatus(TradeStatus.COMPLETED);
+		tradesRepository.save(trade);
 
 		// 각 물건 거래 상태도 sold out 처리
 		trade.getTargetGoods().setGoodsTradeStatus(GoodsTradeStatus.SOLDOUT);
@@ -277,22 +278,24 @@ public class TradesServiceImpl implements TradesService {
 
 	@Override
 	public List<MyRequestDto> getMyRequests() {
-		List<RequestGoodsImageDto> dtoList = tradesRepository.findMyRequests(
+		List<TradeGoodsDao> daoList = tradesRepository.findMyRequests(
 			currentUserService.getCurrentUser().getUsersId());
 
-		return dtoList.stream()
-			.map(dto -> {
-				String requestedGoodsPhotoUrl = getFirstImageUrl(dto.getRequestedGoods());
-				String myGoodsPhotoUrl = getFirstImageUrl(dto.getMyGoods());
+		return daoList.stream()
+			.map(dao -> {
+				String targetGoodsPhotoUrl = getFirstImageUrl(dao.getTargetGoods());
+				String myGoodsPhotoUrl = getFirstImageUrl(dao.getMyGoods());
 
 				return new MyRequestDto(
-					dto.getRequestedGoods().getId(),
-					dto.getRequestedGoods().getTitle(),
-					dto.getRequestedGoods().getPrice(),
-					dto.getRequestedGoods().getCategory().getName(),
-					dto.getRequestedGoods().getPlaceName(),
+					dao.getTargetGoods().getId(),
+					dao.getTargetGoods().getTitle(),
+					dao.getTargetGoods().getPrice(),
+					dao.getTargetGoods().getCategory().getName(),
+					dao.getTargetGoods().getPlaceName(),
 					myGoodsPhotoUrl,
-					requestedGoodsPhotoUrl
+					targetGoodsPhotoUrl,
+					dao.getTargetGoods().getViewCount(),
+					dao.getTargetGoods().getCreatedAt()
 				);
 			})
 			.toList();
@@ -306,7 +309,7 @@ public class TradesServiceImpl implements TradesService {
 	}
 
 	@Transactional
-	private void updateChatroomAndSendChat(ChatRooms chatRooms, Trades trade, ChatType chatType,
+	protected void updateChatroomAndSendChat(ChatRooms chatRooms, Trades trade, ChatType chatType,
 		Goods goodsForMessage) {
 		chatRooms.updateTrade(trade);
 		chatNotificationService.sendTradeRequestChat(chatRooms.getId(), chatType, goodsForMessage);
