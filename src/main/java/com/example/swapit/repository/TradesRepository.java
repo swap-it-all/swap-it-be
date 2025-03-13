e3e7054d16084a11d383986e402f8e2f61d66f0c
HJunng <guswjd1gh@gmail.com>
2025-03-13 15:40:31 +0900

    feat : trade 요청을 받은 건에 내가 trade 요청 불가하도록, 에러 처리.

--- src/main/java/com/example/swapit/repository/TradesRepository.java
+++ src/main/java/com/example/swapit/repository/TradesRepository.java
@@ -1,7 +1,6 @@
 package com.example.swapit.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -10,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.example.swapit.domain.Goods;
 import com.example.swapit.domain.TradeStatus;
 import com.example.swapit.domain.Trades;
-import com.example.swapit.domain.Users;
 import com.example.swapit.domain.dao.TradeGoodsDao;
 import com.example.swapit.domain.dto.trade.InProgressCountDto;
 import com.example.swapit.domain.dto.trade.TradeCountProjection;
@@ -25,27 +23,25 @@ public interface TradesRepository extends JpaRepository<Trades, Long> {
 	void rejectOtherTrades(@Param("targetGoods") Goods targetGoods, @Param("acceptedTradeId") Long acceptedTradeId);
 
 	@Query("SELECT t.targetGoods.id AS goodsId, COALESCE(COUNT(t), 0) AS tradeCount "
-		   + "FROM Trades t "
-		   + "WHERE t.targetGoods.id IN :goodsIds "
-		   + "GROUP BY t.targetGoods.id")
+		+ "FROM Trades t "
+		+ "WHERE t.targetGoods.id IN :goodsIds "
+		+ "GROUP BY t.targetGoods.id")
 	List<TradeCountProjection> findTradeCountByGoodsIds(@Param("goodsIds") List<Long> goodsIds);
 
 	@Query("SELECT new com.example.swapit.domain.dao.TradeGoodsDao(t.targetGoods, t.requestedGoods, t.id) "
-		   + "FROM Trades t "
-		   + "WHERE t.requestedGoods.user.usersId = :userId")
+		+ "FROM Trades t "
+		+ "WHERE t.requestedGoods.user.usersId = :userId")
 	List<TradeGoodsDao> findMyRequests(@Param("userId") Long userId);
 
 	@Query("SELECT t.requestedGoods FROM Trades t WHERE t.targetGoods.id = :goodsId ORDER BY t.createdAt DESC")
 	List<Goods> findGoodsRequests(@Param("goodsId") Long goodsId);
 
-	Optional<Trades> findByRequestedGoodsUserAndTargetGoods(Users requester, Goods targetGoods);
-
-	boolean existsByTargetGoodsAndStatusNot(Goods targetGoods, TradeStatus status);
-
 	@Query("SELECT new com.example.swapit.domain.dto.trade.InProgressCountDto(t.targetGoods.id, COUNT(t)) "
-		   + "FROM Trades t "
-		   + "WHERE t.targetGoods.id IN :goodsIds AND t.status = 'INPROGRESS' "
-		   + "GROUP BY t.targetGoods.id")
+		+ "FROM Trades t "
+		+ "WHERE t.targetGoods.id IN :goodsIds AND t.status = 'INPROGRESS' "
+		+ "GROUP BY t.targetGoods.id")
 	List<InProgressCountDto> findInProgressCountByGoodsIds(@Param("goodsIds") List<Long> goodsIds);
 
+	boolean existsByRequestedGoodsAndTargetGoodsAndStatusNot(Goods requestedGoods, Goods targetGoods,
+		TradeStatus status);
 }
