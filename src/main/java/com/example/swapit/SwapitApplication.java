6b0ec6ffa6f449c146504f33b06fe66e0b805a9b
HJunng <guswjd1gh@gmail.com>
2025-03-15 14:26:25 +0900

    feat : jvm 타임존 강제설정

--- src/main/java/com/example/swapit/SwapitApplication.java
+++ src/main/java/com/example/swapit/SwapitApplication.java
@@ -1,13 +1,25 @@
 package com.example.swapit;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
 public class SwapitApplication {
+
+	@PostConstruct
+	public void init() { // JVM 타임존 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		log.info("JVM TimeZone Set to: {}", TimeZone.getDefault().getID());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwapitApplication.class, args);
