8c3edcbd39a2325cb9c041631bd575c26ea75376
tmfrl99 <99esg@naver.com>
2025-03-20 16:20:08 +0900

    feature: 탈퇴 사유 엔티티, dto 추가

---
+++ src/main/java/com/example/swapit/domain/WithdrawReasons.java
@@ -0,0 +1,48 @@
+package com.example.swapit.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "withdraw_reasons")
+@EntityListeners(AuditingEntityListener.class)
+public class WithdrawReasons {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "reason_id", columnDefinition = "BIGINT", nullable = false)
+	private Long reasonId;
+
+	@Column(name = "reason", columnDefinition = "VARCHAR(200)", nullable = false)
+	private String reason;
+
+	@OneToOne
+	@JoinColumn(name = "users_id", nullable = false)
+	private Users users;
+
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	public WithdrawReasons(String reason, Users users) {
+		this.reason = reason;
+		this.users = users;
+	}
+}
