9d231c5831ddf80c7d9b62ef06c8be0da8673ec6
HJunng <guswjd1gh@gmail.com>
2025-04-21 16:03:01 +0900

    chore : 리프레시 토큰 발급 실패 에러 발생 시, 로그 추가

--- src/main/java/com/example/swapit/service/AuthServiceImpl.java
+++ src/main/java/com/example/swapit/service/AuthServiceImpl.java
@@ -91,6 +91,7 @@ public class AuthServiceImpl implements AuthService {
 		} catch (CustomException ce) {
 			throw ce;
 		} catch (Exception e) {
+			log.error("[리프레시 토큰 발급 에러]", e);
 			throw new CustomException(ErrorCode.NEW_REFRESH_TOKEN_FAIL);
 		}
 	}
