830ca46cefc882a1a2ad1503ca0b45a3447265fe
HJunng <guswjd1gh@gmail.com>
2025-04-23 23:02:50 +0900

    refactor : 알림 구독 확인을 메소드로 분리

--- src/main/java/com/example/swapit/service/notification/NotificationEventListener.java
+++ src/main/java/com/example/swapit/service/notification/NotificationEventListener.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 import com.example.swapit.common.exception.CustomException;
 import com.example.swapit.common.exception.ErrorCode;
+import com.example.swapit.config.websocket.WebsocketDestination;
 import com.example.swapit.domain.NotificationEvent;
 import com.example.swapit.domain.Notifications;
 import com.example.swapit.domain.Users;
@@ -53,33 +54,36 @@ public class NotificationEventListener {
 		// 웹소켓 알림 전송 (앱이 온라인 상태) : "/user/queue/notifications" 구독된 상태
 		NotificationDto notiDto = NotificationDto.of(noti);
 		String userKey = userId.toString();
-		SimpUser simpUser = simpUserRegistry.getUser(userKey);
-		log.debug("[WS 유저 조회] userId={},  simpUser.getName()={}", userKey,
-			simpUser != null ? simpUser.getName() : "없음");
 
-		boolean isSubscribed = false;
+		boolean isNotiSubscribed = isSubscribedToNotifications(userKey);
 
-		if (simpUser != null) {
-			sessionLoop:
-			for (SimpSession session : simpUser.getSessions()) {
-				log.debug("[WS 세션] sessionId={}, subscription 수={}", session.getId(),
-					session.getSubscriptions().size());
-				for (SimpSubscription sub : session.getSubscriptions()) {
-					if ("/user/queue/notifications".equals(sub.getDestination())) {
-						isSubscribed = true;
-						break sessionLoop;
-					}
-				}
-			}
-		}
-
-		if (isSubscribed) {
+		if (isNotiSubscribed) {
 			messagingTemplate.convertAndSendToUser(userKey, "/queue/notifications", notiDto);
 			log.debug("[WS알림 전송] 유저id={}, payload={} ", userId, notiDto);
 		} else {
 			log.debug("[WS알림 스킵] 유저id={}는 현재 오프라인 상태 → FCM 전송", userId);
 			sendFcm(user, noti);
 		}
+	}
+
+	private boolean isSubscribedToNotifications(String userKey) {
+		SimpUser user = simpUserRegistry.getUser(userKey);
+		log.debug("[WS 유저 조회] userId={}, simpUser.getName()={}", userKey,
+			user != null ? user.getName() : "없음");
+
+		if (user == null)
+			return false;
+
+		for (SimpSession session : user.getSessions()) {
+			log.debug("[WS 세션] sessionId={}, subscription 수={}", session.getId(),
+				session.getSubscriptions().size());
+			for (SimpSubscription sub : session.getSubscriptions()) {
+				if (WebsocketDestination.NOTIFICATION_QUEUE.getPath().equals(sub.getDestination())) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	private void sendFcm(Users user, Notifications noti) {
