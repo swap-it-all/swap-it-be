94a1be3ce05075008de5f0c243ccfec3dff7b7fd
tmfrl99 <99esg@naver.com>
2025-04-23 20:38:54 +0900

    fix: jwt token claim 이메일을 id로 변경

--- src/main/java/com/example/swapit/config/security/CustomUserDetails.java
+++ src/main/java/com/example/swapit/config/security/CustomUserDetails.java
@@ -27,7 +27,7 @@ public class CustomUserDetails implements UserDetails {
 
 	@Override
 	public String getUsername() {
-		return user.getEmail();
+		return user.getUsersId().toString();
 	}
 
 	@Override
--- src/main/java/com/example/swapit/repository/TokensRepository.java
+++ src/main/java/com/example/swapit/repository/TokensRepository.java
@@ -9,7 +9,7 @@ import com.example.swapit.domain.Tokens;
 import com.example.swapit.domain.Users;
 
 public interface TokensRepository extends JpaRepository<Tokens, Long> {
-	Optional<Tokens> findByUserEmail(String email);
+	Optional<Tokens> findByUserUsersId(Long usersId);
 
 	Optional<Tokens> findByRefreshToken(String refreshToken);
 
