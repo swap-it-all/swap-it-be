37a21a21835c67dd9dba91406b34ffd8ce0cbeea
tmfrl99 <99esg@naver.com>
2025-06-18 13:41:11 +0900

    fix: 카카오 탈퇴는 revoke 필요

--- src/main/java/com/example/swapit/service/AuthServiceImpl.java
+++ src/main/java/com/example/swapit/service/AuthServiceImpl.java
@@ -242,7 +242,7 @@ public class AuthServiceImpl implements AuthService {
 			throw new CustomException(ErrorCode.EXIST_INPROGRESS_TRADE);
 		}
 
-		// callKakaoRevoke(kakaoToken);
+		callKakaoRevoke(kakaoToken);
 		userWithdraw(reason, user);
 	}
 
